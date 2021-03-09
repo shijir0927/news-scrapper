a9d6158a52470cf3ce7cc4723d0bdd894f0fe742
Anuujin Purevod <anuujinpurevod@anuujins-mbp.attlocal.net>
2021-03-09 09:54:14 -0800

    created search bar

--- app/public/components/header.js
+++ app/public/components/header.js
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Header = ()=>{
     return(
-        <div className='header-container'>
+        <div className ='header-container'>
             <h1>The Hacker News</h1>
-            <input className='header-search' placeholder='Search...'> </input>
-
+            <input className ='header-search' placeholder ='Search...'/>
         </div>
     )
 }
