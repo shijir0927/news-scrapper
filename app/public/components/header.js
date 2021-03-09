b460299725af5faaf4c163ab8469fceac6b03260
Anuujin Purevod <anuujinpurevod@anuujins-mbp.attlocal.net>
2021-03-09 08:57:57 -0800

    created the search bar

--- app/public/components/header.js
+++ app/public/components/header.js
@@ -4,6 +4,7 @@ const Header = ()=>{
     return(
         <div className='header-container'>
             <h1>The Hacker News</h1>
+            <input className='header-search' placeholder='Search...'> </input>
 
         </div>
     )
--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/header'
+
 function App() {
   return (
     <div className="App">
