ee8e4b0c07c240b1d197521848ef36f9334c15b6
Shijir <shijiree_mng@yahoo.com>
2021-03-16 08:43:00 -0700

    created the newsItem component

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,30 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
-import Header from './components/header'
+import Header from './components/header';
+import NewsItem from './components/newsItem';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+
+    fetch('http://hn.algolia.com/api/v1/search?query='+'java')
+    .then((data)=>{
+      return data.json();
+    }).then((result)=>{
+      console.log("result: ",result);
+      setData(result)
+    })
+
+  }, [])
+
   return (
     <div className="App">
       <Header/>
+      {data.hits !== undefined && data.hits.map((item)=>{
+        return <NewsItem author={item.author} title={item.title} url={item.url}/>
+      })}
     </div>
   );
 }
