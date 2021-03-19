320a43a15de2173be83adf73839541ee3fc253e9
Anuujin Purevod <anuujinpurevod@anuujins-mbp.attlocal.net>
2021-03-19 09:57:24 -0700

    aaaa

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+import Header from './components/header';
+import NewsItem from './components/newsItem';
+
+function App() {
+
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('java');
+  const [recents, setRecents] = useState([]);
+
+  useEffect(()=>{
+
+    fetch('http://hn.algolia.com/api/v1/search?query='+search)
+    .then((data)=>{
+      return data.json();
+    }).then((result)=>{
+      setData(result)
+    })
+
+    fetch('https://hn.algolia.com/api/v1/search?tags=front_page')
+    .then((data)=>{
+      return data.json();
+    }).then((result)=>{
+      setRecents(result)
+    })
+
+  }, [search])
+
+  const setSearchValue = (value) =>{
+    setSearch(value);
+  }
+
+  return (
+    <div className="App">
+      <Header setSearchValue={setSearchValue}/>
+      <div className="news-container">
+        <div className="news-container-left">
+          {data.hits !== undefined && data.hits.map((item)=>{
+            return <NewsItem author={item.author} title={item.title} url={item.url}/>
+          })}
+        </div>
+        <div className="news-container-right">
+          <h3>Most Recents</h3>
+          {recents.hits !== undefined && recents.hits.slice(0, 6).map((item)=>{
+            return <NewsItem author={item.author} title={item.title} url={item.url}/>
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = ({setSearchValue})=>{
+  return(
+    <div className='header-container'>
+      <div className='header-inner'>
+        <h1>The Hacker News</h1>
+        <input className='header-search' placeholder='Search...' onChange={(e)=>setSearchValue(e.target.value)}/>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
